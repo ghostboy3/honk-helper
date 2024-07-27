88e8de711efe6ca72d99ca683cf7bb9f12ef0d05
unknown <nico.luu.to@gmail.com>
2024-07-27 18:54:26 -0400

    time count working

--- content.js
+++ content.js
@@ -1,20 +1,4 @@
-// // Adding Goose
-// document.head.appendChild(styleElement);
 
-// var goose = document.createElement('div');
-// var s = goose.style;
-
-// s.position = 'fixed';
-// s.top = '100px';
-// s.left = '100px';
-// s.width = '100px';
-// s.height = '100px';
-// s.pointerEvents = 'none';
-// s.backgroundColor = 'red';
-
-// goose.id = "goose";
-// console.log("honk");
-// document.body.appendChild(goose);
 console.log('HIIII');
 const img = document.createElement('img');
 img.src = chrome.runtime.getURL('gooseIcon.jpg');
