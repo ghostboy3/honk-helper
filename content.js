7b0afb54c695f60e3ac7f921d207577264af2e10
unknown <nico.luu.to@gmail.com>
2024-07-27 19:00:44 -0400

    testing

--- content.js
+++ content.js
@@ -1,5 +1,3 @@
-
-console.log('HIIII');
 const img = document.createElement('img');
 img.src = chrome.runtime.getURL('gooseIcon.jpg');
 img.style.position = 'fixed';
@@ -9,8 +7,4 @@ img.style.zIndex = '1000';
 img.style.width = '100px';
 img.style.height = 'auto';
 img.id = 'goose'
-
-
-console.log("HIIIIIIIIIIIIII");
-// document.head.appendChild(<h1>HIJDFSKLFJK</h1>);
 document.body.appendChild(img);
