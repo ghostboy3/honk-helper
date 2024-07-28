f4cc5c4a86e0c74b4e16e947a81fdce6fb44d3b3
unknown <nico.luu.to@gmail.com>
2024-07-27 20:24:40 -0400

    blocked sites

--- background.js
+++ background.js
@@ -7,19 +7,23 @@ chrome.runtime.onInstalled.addListener(() => {
     });
   });
 });
-
+// timer
 chrome.tabs.onActivated.addListener(async (activeInfo) => {
   const tab = await chrome.tabs.get(activeInfo.tabId);
   if (tab.url) {
-    trackTime(tab.url);
+    checkAndTrackTime(tab.url);
   }
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete' && tab.url) {
-    trackTime(tab.url);
+    checkAndTrackTime(tab.url);
   }
 });
+
+function checkAndTrackTime(url) {
+  trackTime(url);
+}
 
 function trackTime(url) {
   const now = new Date().getTime();
@@ -33,58 +37,51 @@ function trackTime(url) {
   });
 }
 
-// // // Track time
-// chrome.storage.local.set({"HIII": "huulooooopooooooooooo"})
-// let currentWebsite = '';
-// let startTime = 0;
+chrome.storage.onChanged.addListener((changes, areaName) => {
+  if (areaName === 'local' && changes.badWebsites) {
+    updateRules();
+  }
+});
 
-// chrome.tabs.onActivated.addListener(activeInfo => {
-//   chrome.tabs.get(activeInfo.tabId, tab => {
-//     handleWebsiteChange(tab.url);
-//   });
+function updateRules() {
+  chrome.storage.local.get("badWebsites", (data) => {
+    const badWebsites = data.badWebsites || [];
+    const rules = badWebsites.map((badUrl, index) => ({
+      id: index + 1,
+      priority: 1,
+      action: { type: "block" },
+      condition: { urlFilter: badUrl, resourceTypes: ["main_frame"] }
+    }));
+    chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: rules.map(rule => rule.id),
+      addRules: rules
+    });
+  });
+}
+
+updateRules();
+
+// chrome.tabs.onActivated.addListener(async (activeInfo) => {
+//   const tab = await chrome.tabs.get(activeInfo.tabId);
+//   if (tab.url) {
+//     trackTime(tab.url);
+//   }
 // });
 
 // chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//   if (changeInfo.status === 'complete') {
-//     handleWebsiteChange(tab.url);
+//   if (changeInfo.status === 'complete' && tab.url) {
+//     trackTime(tab.url);
 //   }
 // });
 
-// function handleWebsiteChange(url) {
-//   const website = new URL(url).hostname;
-
-//   if (website !== currentWebsite) {
-//     if (currentWebsite) {
-//       const timeSpent = (Date.now() - startTime) / 1000;
-//       saveTimeSpent(currentWebsite, timeSpent);
+// function trackTime(url) {
+//   const now = new Date().getTime();
+//   chrome.storage.local.get(["currentWebsite", "startTime"], (data) => {
+//     const { currentWebsite, startTime } = data;
+//     if (currentWebsite && startTime) {
+//       const timeSpent = now - startTime;
+//       console.log(`Time spent on ${currentWebsite}: ${timeSpent} ms`);
 //     }
-
-//     currentWebsite = website;
-//     startTime = Date.now();
-//     chrome.storage.local.set({currentWebsite, startTime}).then((result) => {
-//       console.log("Value is " + result.key);
-//     });
-//   }
-// }
-
-// function saveTimeSpent(website, timeSpent) {
-//   chrome.storage.local.get([website], data => {
-//     const totalTime = (data[website] || 0) + timeSpent;
-//     chrome.storage.local.set({[website]: totalTime})({currentWebsite, startTime}).then((result) => {
-//       console.log("Value is " + result.key);
-//     });
+//     chrome.storage.local.set({ currentWebsite: url, startTime: now });
 //   });
 // }
-
-// chrome.runtime.onStartup.addListener(() => {
-//   chrome.storage.local.get(['currentWebsite', 'startTime'], data => {
-//     if (data.currentWebsite && data.startTime) {
-//       currentWebsite = data.currentWebsite;
-//       startTime = data.startTime;
-//     }
-//   });
-// });
-
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.storage.local.clear();
-// });
