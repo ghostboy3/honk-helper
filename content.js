64b71ab55ecf5732010db20369246a6e1e5da91b
KennethWang1 <122188744+KennethWang1@users.noreply.github.com>
2024-07-28 02:35:52 -0400

    Add files via upload

--- content.js
+++ content.js
@@ -6,11 +6,17 @@ const importfr = (path) => {
 }
 
 const Honk = new Audio(importfr('honk.mp3'));
-
 const goosL = chrome.runtime.getURL('assets/goosL.png');
 const goosR = chrome.runtime.getURL('assets/goosR.png');
 const GoosWalkingL = chrome.runtime.getURL('assets/GoosWalkingL.gif');
 const GoosWalkingR = chrome.runtime.getURL('assets/GoosWalkingR.gif');
+const lieDownL = importfr('/lieDownL.gif');
+const lieDownR = importfr('/lieDownR.gif');
+const wakeUpL = importfr('/wakeUpL.gif');
+const wakeUpR = importfr('/wakeUpR.gif');
+const sleepL = importfr('/sleepL.gif');
+const sleepR = importfr('/sleepR.gif');
+var asleep = false;
 var endPic = goosL;
 const img = document.createElement('img');
 img.src = goosL;
@@ -83,22 +89,56 @@ function resetGoose(img){
 }
 resetGoose(img)
 
-setInterval(() =>{
-  // resetGoose(img)
-  if (document.hasFocus()) {
+function sleep(){
+    console.log(endPic.substring(endPic.length-1));
+    asleep = true;
 
-  console.log("moved");
+    if(endPic == goosL){
+        img.src = lieDownL;
+        console.log('zz');
+        setTimeout(() =>{
+            img.src = sleepL;
+        },500);
+        setTimeout(() =>{
+            console.log('wake up');
+            img.src = wakeUpL;
+        },540000);
+    }else{
+        img.src = lieDownR;
+        console.log('zz');
+        setTimeout(() =>{
+            img.src = sleepR;
+        },500);
+        setTimeout(() =>{
+            console.log('wake up');
+            img.src = wakeUpR;
+        },540000);
+    }
+    setTimeout(() =>{
+        img.src = endPic;
+    },541000);
+}
+
+setInterval(() => {
+  // resetGoose(img)
+  if (document.hasFocus() && !asleep) {
+    console.log("moved");
     moveGoose(img,Math.floor(Math.random()*(window.innerWidth/7)), Math.floor(Math.random()*(window.innerHeight/7)));
+  }
 }, 5000)
 
 setInterval(() => {
     chrome.storage.local.get(["currentWebsite", "startTime"], (data) => {
         const { currentWebsite, startTime } = data;
         console.log(new URL(currentWebsite).hostname);
-        if(new Date().getTime() - startTime > 600000 && blacklist.includes(new URL(currentWebsite).hostname)) {
-            honk();
-            startTime = startTime - 2000;
-            chrome.storage.local.set({ startTime: startTime });
+        if(blacklist.includes(new URL(currentWebsite).hostname)){
+            if(new Date().getTime() - startTime > 600000) {
+                honk();
+                startTime = startTime - 2000;
+                chrome.storage.local.set({ startTime: startTime });
+            }else if(new Date().getTime() - startTime < 101){
+                sleep();
+            }
         }
     })
 }, 100)
