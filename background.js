2834cc15d8378833d2dbd761bcc985f14d719e5c
unknown <nico.luu.to@gmail.com>
2024-07-27 23:18:00 -0400

    moving works

--- background.js
+++ background.js
@@ -63,27 +63,3 @@ function updateRules() {
 
 updateRules();
 
-// chrome.tabs.onActivated.addListener(async (activeInfo) => {
-//   const tab = await chrome.tabs.get(activeInfo.tabId);
-//   if (tab.url) {
-//     trackTime(tab.url);
-//   }
-// });
-
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//   if (changeInfo.status === 'complete' && tab.url) {
-//     trackTime(tab.url);
-//   }
-// });
-
-// function trackTime(url) {
-//   const now = new Date().getTime();
-//   chrome.storage.local.get(["currentWebsite", "startTime"], (data) => {
-//     const { currentWebsite, startTime } = data;
-//     if (currentWebsite && startTime) {
-//       const timeSpent = now - startTime;
-//       console.log(`Time spent on ${currentWebsite}: ${timeSpent} ms`);
-//     }
-//     chrome.storage.local.set({ currentWebsite: url, startTime: now });
-//   });
-// }
